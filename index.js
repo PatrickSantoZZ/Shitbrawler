462aa9e8700883c57de0dc8cdf7be667ef1232b3
Fukki <takungwong@gmail.com>
2019-01-04 12:45:11 +0700

    :x

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 module.exports = function Shitbrawler(mod) {
 	const cmd = mod.command || mod.require.command;
-	let config = {}, gameId = null, enabled = false, isSorc = false, inBuff = false, itemCd = {brooch: 0, rootbeer: 0};
+	let config = {}, gameId = null, enabled = false, isBrawler = false, itemCd = {brooch: 0, rootbeer: 0};
 	try {
 		config = require('./config.json'); }
 	catch(e) { 
@@ -13,45 +13,30 @@ module.exports = function Shitbrawler(mod) {
 		fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config, null, 2));
 	}
 	
-	cmd.add('sorc', (arg) => {
-		if (!isSorc) {
-			msg(`You are not sorc`);
+	cmd.add('shitbrawler', (arg) => {
+		if (!isBrawler) {
+			msg(`You are not brawler`);
 		} else {
 			enabled = !enabled;
-			msg(`Shitsorc now ${enabled ? 'Enabled' : 'Disabled'}`);
+			msg(`Shitbrawler now ${enabled ? 'Enabled' : 'Disabled'}`);
 		}
 	});
 	
 	mod.hook('S_LOGIN', 12, (event) => {
 		gameId = event.gameId;
-		isSorc = (event.templateId - 10101) % 100 === 4;
-		if (isSorc) enabled = true;
+		isBrawler = (event.templateId - 10101) % 100 === 10;
+		if (isBrawler) enabled = true;
 		else enabled = false;
-	});
-	
-	mod.hook('C_PRESS_SKILL', 4, (event) => {
-		if(!enabled || !inBuff) return;
-		let now = Date.now();
- 		if (now > itemCd.brooch) useItem(config.brooch);
- 		if (now > itemCd.rootbeer) useItem(config.rootbeer);
-		inBuff = false;
 	});
 
 	mod.hook('S_ABNORMALITY_BEGIN', 3, (event) => {
 		if(!enabled) return;
-		if(event.target === gameId && event.id === 500150)
-			inBuff = true;
-		
+		if(event.target === gameId && event.id === 10153210)  {
 			let now = Date.now();
  			if (now > itemCd.brooch) useItem(config.brooch);
  			if (now > itemCd.rootbeer) useItem(config.rootbeer);
+		}
 	});
-	
-	mod.hook('S_ABNORMALITY_END', 1, (event) => {
-		if(!enabled) return;
-		if(event.target === gameId && event.id === 500150)
-			inBuff = false;
-	}
 	
 	mod.hook('S_START_COOLTIME_ITEM', 1, {order: Number.NEGATIVE_INFINITY}, event => {
  		if(!enabled) return;
